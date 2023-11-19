3e5dbdc01fc6b09a282733718875a4db4566a95f
Adisirinli <konulfma@code.edu.az>
2023-11-19 15:08:30 +0400

    Add project files.

---
+++ calculation/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+DateTime now = DateTime.Now;
+Console.WriteLine("hazirki vaxt.." + now);
+
+Console.WriteLine("ili daxil edin");
+int year = int.Parse(Console.ReadLine());
+
+while (true)
+{
+    if (year == now.Year)
+    {
+        Console.WriteLine("duzgun daxil etdiniz");
+        break;
+
+    }
+    else
+    {
+
+        Console.WriteLine("sef daxil etdiniz...");
+       
+        year = int.Parse(Console.ReadLine());
+
+    }
+}
